61ae85999888bed7c3b38f8816f01372da1c6da4
Daniel Lopez <daniellopez@nevada.unr.edu>
2021-02-22 01:16:35 -0800

    buttons can raise now

--- src/components/RoundButton.jsx
+++ src/components/RoundButton.jsx
@@ -3,15 +3,19 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components'
 
 const Button = styled.button`
-    height:50px;
-    width:140px;
+    height:40px;
+    width:135px;
     border-radius:21px;
     border:none;
-    box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.15);
+    box-shadow: 7px 7px 5px rgba(0, 0, 0, 0.25);
+    transition: transform, 0.5s;
+    transition: box-shadow, 0.5s;
+    font-family: 'Avenir', Helvetica, sans-serif;
+
     &:hover {
-        box-shadow: 10px 10px 6px rgba(0, 0, 0, 0.15);
-        transition: 0.5s;
-        border: 1px;
+        position:relative;
+        box-shadow: 7px 7px 5px rgba(0, 0, 0, 0.15);
+        transform: translate(-3px, -3px);
     }
 `
 

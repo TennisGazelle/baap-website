2205cf201f0ad69296ed21fe16032c3c7477e7b4
Daniel Lopez <daniellopez@nevada.unr.edu>
2021-02-25 21:15:18 -0800

    init config page setup; header in both

---
+++ src/components/ConfigPage.jsx
@@ -0,0 +1,11 @@
+import { Component } from 'react'
+import Header from './Header'
+
+
+export class ConfigPage extends Component {
+    render() {
+        return <div>
+            <Header color='black'/>
+        </div>
+    }
+}
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,6 +5,7 @@ import tennisgazelle_logo from '../imgs/tennisgazelle_logo.png';
 import styled from 'styled-components'
 import RoundButton, { ButtonHolder } from './RoundButton'
 import links from './Links.json'
+import Header from './Header';
 
 const ImgContainer = styled.div`
     position: relative;
@@ -82,40 +83,42 @@ const TitleHolder = styled.div`
 export class LandingPage extends Component {
     render() {
         return <div>
-            <ButtonHolder style={{ justifyContent:'flex-end', minHeight: '40px'}}>
-                {/* <RoundButton style={{ order: 1 }} color="orange" onClick={() => {console.log('clicked orange')}}>LOG IN</RoundButton>
-                <RoundButton style={{ order: 2 }} color="red" onClick={() => {console.log('clicked red')}}>SIGN UP</RoundButton> */}
-            </ButtonHolder>
+            {/* <ButtonHolder style={{ justifyContent:'flex-end', minHeight: '40px'}}>
+                <RoundButton style={{ order: 1 }} color="orange" onClick={() => {console.log('clicked orange')}}>LOG IN</RoundButton>
+                <RoundButton style={{ order: 2 }} color="red" onClick={() => {console.log('clicked red')}}>SIGN UP</RoundButton>
+            </ButtonHolder> */}
+
+            <Header isInvisible />
 
             <br/>
 
-        <ContentHolder>
-            <ImgContainer>
-                <PipelineImage src={pipeline_logo} alt="Background Image of Pipeline" />
-                <SubImgContainer>
-                    <a href={links.blender_home} target="_blank" rel="noreferrer">
-                        <BlenderImage src={blender_logo} alt="Blender Logo"/>
-                    </a>
-                    <a href={links.tennisgazelle_home} target="_blank" rel="noreferrer">
-                        <TGImage src={tennisgazelle_logo} alt="Website Author Logo" />
-                    </a>
-                </SubImgContainer>
-            </ImgContainer>
-            <TitleHolder>
-                <h1>BAAP</h1>
-                <h2>Blender as a Pipeline</h2>
-                <ButtonHolder >
-                    <RoundButton color="BLACK" disabled>ONBOARD</RoundButton>
-                    <a href={links.github.latest_release}>
-                        <RoundButton color="BLACK" >DOWNLOAD</RoundButton>
-                    </a>
-                    <a href={links.github.main} target="_blank" rel="noreferrer">
-                        <RoundButton color="BLACK">SOURCE</RoundButton>
-                    </a>
-                </ButtonHolder>
-            </TitleHolder>
-        </ContentHolder>
-        </div>        
+            <ContentHolder>
+                <ImgContainer>
+                    <PipelineImage src={pipeline_logo} alt="Background Image of Pipeline" />
+                    <SubImgContainer>
+                        <a href={links.blender_home} target="_blank" rel="noreferrer">
+                            <BlenderImage src={blender_logo} alt="Blender Logo"/>
+                        </a>
+                        <a href={links.tennisgazelle_home} target="_blank" rel="noreferrer">
+                            <TGImage src={tennisgazelle_logo} alt="Website Author Logo" />
+                        </a>
+                    </SubImgContainer>
+                </ImgContainer>
+                <TitleHolder>
+                    <h1>BAAP</h1>
+                    <h2>Blender as a Pipeline</h2>
+                    <ButtonHolder>
+                        <RoundButton color="BLACK" disabled>ONBOARD</RoundButton>
+                        <a href={links.github.latest_release}>
+                            <RoundButton color="BLACK" >DOWNLOAD</RoundButton>
+                        </a>
+                        <a href={links.github.main} target="_blank" rel="noreferrer">
+                            <RoundButton color="BLACK">SOURCE</RoundButton>
+                        </a>
+                    </ButtonHolder>
+                </TitleHolder>
+            </ContentHolder>
+        </div>
     }
 }
 

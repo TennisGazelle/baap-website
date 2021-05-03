4d26624f5b2f96768edfa8700497f01714556783
Daniel Lopez <daniellopez@nevada.unr.edu>
2021-05-03 01:19:00 -0700

    config page squished for world view; config page choices

--- src/components/ConfigTemplateCard.jsx
+++ src/components/ConfigTemplateCard.jsx
@@ -1,16 +1,22 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-
-// const images = require.context('../../public/imgs')
+import SplitPane from 'react-split-pane';
+import videoEditingBlenderPic from '../imgs/blender_video_edit.png';
+import modelingPic from '../imgs/modeling.jpeg';
+import blenderPic from '../imgs/blender_use.png';
 
 const Card = styled.div`
-    height:  400px;
-    width:   490px;
+    height:  250px;
+    width:   400px;
+    border-radius: 20px;
     box-shadow: 7px 7px 5px rgba(0, 0, 0, 0.25);
     transition: transform, 0.5s;
     transition: box-shadow, 0.5s;
     font-family: 'Avenir', Helvetica, sans-serif;
+    margin: 5px 10px;
+    text-align: center;
+    outline: 5px  black;
 
     &:hover {
         position: relative;
@@ -19,21 +25,36 @@ const Card = styled.div`
     }
 `
 
-class ConfigTemplateCard extends Component {
-    render() {
-        // let imageData = import('../imgs/blender_video_edit_pic.jpeg')
-        const imageData = require('imgs/blender_video_edit.png');
-
-        const HeaderImage = styled.img`
-            display: block;
-            width: 100%;
-            height: auto;
-            src: url('${imageData}')
+const WhiteText = styled.h1`
+    font-size: 50px;
+    background-color:grey;
+    color:white;
 `
 
+class ConfigTemplateCard extends Component {
+    render() {
+        const pic = require('../imgs/blender_video_edit.png')
+        console.log(pic)
+        console.log(this.props.details)
+
+        var backgroundImage;
+
+        if (this.props.details.cardKey === 'vfx') {
+            backgroundImage = <img src={videoEditingBlenderPic} class="center" width="200" alt={this.props.details.imgAlt}/>
+        } else if (this.props.details.cardKey === 'model') {
+            backgroundImage = <img src={modelingPic} width="200" class="center" alt={this.props.details.imgAlt}/>
+        } else if (this.props.details.cardKey === 'blender') {
+            backgroundImage = <img src={blenderPic} width="200" class="center" alt={this.props.details.imgAlt}/>
+        }
+
         return <Card>
-            {/* <img src={imageData} alt="video editing pic"/> */}
-            <HeaderImage/>
+            <SplitPane split="horizontal">
+                {/* <Pane initialSize="200px">You can use a Pane component</Pane> */}
+                {backgroundImage}
+                <WhiteText>{this.props.details.cardTitle}</WhiteText>
+                    <i style={{display: "inline-block"}}>{this.props.details.cardDescription}</i>
+             </SplitPane>
+
         </Card>
     }
 }
@@ -41,8 +62,12 @@ class ConfigTemplateCard extends Component {
 ConfigTemplateCard.propTypes = {
     details: PropTypes.exact({
         cardTitle: PropTypes.string.isRequired,
-        cardDescription: PropTypes.string.isRequired
-    }).isRequired
+        cardDescription: PropTypes.string.isRequired,
+        cardKey: PropTypes.string.isRequired,
+        imgAlt: PropTypes.string.isRequired
+    }).isRequired,
+    image: PropTypes.node,
+    selected: PropTypes.bool
 }
 
 export default ConfigTemplateCard;

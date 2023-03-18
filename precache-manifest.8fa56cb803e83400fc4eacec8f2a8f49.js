0fca5ffd17238eb0771fee7277e769f63b839860
TennisGazelle <TennisGazelle@users.noreply.github.com>
2023-03-18 00:53:42 +0000

    deploy: f175ba98479ec0776fb353082bbdce5f5427d890

---
+++ precache-manifest.8fa56cb803e83400fc4eacec8f2a8f49.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "59cba1d457b01522cfa3",
+    "url": "/static/css/main.f08a7005.chunk.css"
+  },
+  {
+    "revision": "59cba1d457b01522cfa3",
+    "url": "/static/js/main.036757ed.chunk.js"
+  },
+  {
+    "revision": "8dc879ecda24a048a842",
+    "url": "/static/js/runtime~main.730db379.js"
+  },
+  {
+    "revision": "b9b4c58e71d35a11b9d7",
+    "url": "/static/js/2.5c576955.chunk.js"
+  },
+  {
+    "revision": "8bb124ea66fc9aa92449",
+    "url": "/static/js/3.362f479d.chunk.js"
+  },
+  {
+    "revision": "0f367020eae5d8818d50f4c5b392b1e6",
+    "url": "/static/media/pipeline.0f367020.png"
+  },
+  {
+    "revision": "a065499d23e8da566409fc80f109bb00",
+    "url": "/static/media/blender_logo.a065499d.svg"
+  },
+  {
+    "revision": "c8585c2953a4df40cbf98aa6a2173332",
+    "url": "/static/media/tennisgazelle_logo.c8585c29.png"
+  },
+  {
+    "revision": "2b96f84e787a4547cb0edf1a8c971636",
+    "url": "/index.html"
+  }
+];
